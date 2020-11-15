2deac6265af64be973dc080ad873b2ef3cb6b787
Andrew Guest <andrew@aguest.me>
2020-11-14 18:18:38 -0600

    Setting up Docker with cookiecutter

--- {{cookiecutter.project_slug}}/app/main.py
+++ {{cookiecutter.project_slug}}/app/main.py
@@ -1,6 +1,10 @@
+import os
+
 from fastapi import FastAPI
 from motor import motor_asyncio
 
 
+mongodb_servername = os.getenv("MONGODB_SERVNAME")
+
 api = FastAPI()
-mongodb_connection = motor_asyncio.AsyncIOMotorClient({{ cookiecutter.project_slug }}_db)
+mongodb_connection = motor_asyncio.AsyncIOMotorClient(mongodb_servername, {{ cookiecutter.mongodb_port }})
