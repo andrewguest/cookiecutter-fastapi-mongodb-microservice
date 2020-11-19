e43918e186e18a352ffc75e4bd328120dd7e2b82
Andrew <andrew@aguest.me>
2020-11-18 23:56:16 -0600

    Finished configuring cookiecutter for MongoDB

--- {{cookiecutter.microservice_slug}}/routes/ping.py
+++ {{cookiecutter.microservice_slug}}/routes/ping.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app import {{ cookiecutter.microservice_slug }}_api
+from app import {{cookiecutter.microservice_slug}}_api
 
 
 router = APIRouter()
@@ -14,7 +14,7 @@ def ping():
 @router.get('/mongo', status_code=200)
 async def test_mongo():
     data = []
-    collection = {{ cookiecutter.microservice_slug }}_api.mongodb_connection['test']['test_collection']
+    collection = {{cookiecutter.microservice_slug}}_api.mongodb_connection['{{cookiecutter.mongodb_database}}']['{{cookiecutter.mongodb_collection}}']
     async for result in collection.find():
         del result['_id']
         data.append(result)
