4a8b6d613709d827b8191586a4badc0aa24672c3
Andrew <andrew@aguest.me>
2020-12-10 22:07:23 -0600

    Update mongo_config.py

--- {{cookiecutter.microservice_slug}}/database/mongo_config.py
+++ {{cookiecutter.microservice_slug}}/database/mongo_config.py
@@ -5,6 +5,6 @@ class ExampleDatabaseSettings(BaseSettings):
     DB_URL = "mongodb://{{cookiecutter.mongodb_new_username}}:{{cookiecutter.mongodb_new_username_password}}@{{cookiecutter.mongodb_container_name}}:27017/{{cookiecutter.mongodb_database}}?authSource={{cookiecutter.mongodb_database}}"
     DB_NAME = "{{cookiecutter.mongodb_database}}"
     COLLECTION_NAME = "{{cookiecutter.mongodb_collection}}"
-    
-    
+
+
 example_db_settings = ExampleDatabaseSettings()
